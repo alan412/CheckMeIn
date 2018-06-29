ec734713023dacb102ad8f25fef06af4440719d9
Alan Smith <alan@randomsmiths.com>
2018-06-29 11:15:28 -0400

    Added in shop steward and keyholder functionality

--- checkMeIn.py
+++ checkMeIn.py
@@ -4,28 +4,70 @@ import argparse
 from members import Members
 import datetime
 
+KEYHOLDER_BARCODE = '999901'
+SHOP_STEWARD_BARCODE = '999902'
+
 class CheckMeIn(object):
    def __init__(self):
       self.lookup = TemplateLookup(directories=['HTMLTemplates'],default_filters=['h'])
       self.members = Members('data/members.csv', 'TFI Barcode', 'TFI Display Name');
+      self.keyholder_name = 'N/A';
+      self.shop_steward_name = 'N/A';
 
    def template(self, name, **kwargs):
       return self.lookup.get_template(name).render(**kwargs);
 
    @cherrypy.expose
    def station(self,error=''):
-      return self.template('station.html',members=self.members,error=error)
+      return self.template('station.html',members=self.members,
+                           keyholder_name=self.keyholder_name,shop_steward_name=self.shop_steward_name, 
+                           error=error)
 
    @cherrypy.expose
    def who_is_here(self):
       return self.template('who_is_here.html',now=datetime.datetime.now(), whoIsHere=self.members.whoIsHere())
+
+   @cherrypy.expose
+   def keyholder(self, barcode):
+      error = ''
+      barcode = barcode.strip();
+      if barcode == KEYHOLDER_BARCODE:
+          self.keyholder_name = 'N/A';
+          self.members.emptyBuilding();
+      else:
+          result = self.members.getName(barcode);
+          if result.startswith('Invalid'):
+              return self.template('keyholder.html', error=result);
+          self.keyholder_name = result
+      return self.station();
+
+   @cherrypy.expose
+   def shop_steward(self, barcode):
+      error = ''
+      barcode = barcode.strip();
+      if barcode == SHOP_STEWARD_BARCODE:
+          self.shop_steward_name = 'N/A';
+      else:
+          result = self.members.getName(barcode);
+          if result.startswith('Invalid'):
+              return self.template('shop_steward.html', error=result);
+          self.shop_steward_name = result
+      return self.station();
    
    @cherrypy.expose
    # later change this to be more ajaxy, but for now...
    def scanned(self, barcode):
-      error = self.members.scanned(barcode[:6]); 
+      error = ''
+# strip whitespace before or after barcode digits (occasionally a space comes before or after
+      barcode = barcode.strip();
+      if barcode == KEYHOLDER_BARCODE:
+         return self.template('keyholder.html'); 
+      elif barcode == SHOP_STEWARD_BARCODE:
+         return self.template('shop_steward.html');
+      else:
+         error = self.members.scanned(barcode); 
       return self.station(error);
-       
+      
    @cherrypy.expose
    def index(self):
       return self.who_is_here(); 
