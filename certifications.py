9083590f735e674be41c06eca6d83c3a19671730
Alan Smith <alan.smith@nutanix.com>
2019-06-15 08:10:49 -0400

    initial parsing of csv, and setting up tables

---
+++ certifications.py
@@ -0,0 +1,106 @@
+import sqlite3
+import os
+import csv
+from enum import IntEnum
+
+
+class CertificationLevels(IntEnum):
+    none = 0
+    basic = 1
+    certified = 10
+    dof = 20
+    instructor = 30
+    certifier = 40
+
+
+class Certifications(object):
+    def __init__(self, database):
+        self.database = database
+
+    def createTable(self):
+        with sqlite3.connect(self.database) as c:
+            self.migrate(c, 0)
+
+    def addTool(self, dbConnection, tool_id, grouping, name, restriction=0, comments=''):
+        dbConnection.execute('INSERT INTO tools VALUES(?,?,?,?,?)',
+                             (tool_id, grouping, name, restriction, comments))
+
+    def migrate(self, dbConnection, db_schema_version):
+        if db_schema_version < 8:
+            dbConnection.execute('''CREATE TABLE restrictions
+                                    (id        INTEGER PRIMARY KEY,
+                                     descr     TEXT)''')
+            dbConnection.execute('''INSERT INTO restrictions VALUES(
+                                1, "Over 18 Only")''')
+
+            dbConnection.execute('''CREATE TABLE tools
+                                 (id           INTEGER PRIMARY KEY,
+                                  grouping     TEXT,
+                                  name         TEXT,
+                                  restriction  INTEGER DEFAULT 0,
+                                  comments     TEXT)''')
+
+            self.addTool(dbConnection, 1, 1, "Sheet Metal Brake")
+            self.addTool(dbConnection, 2, 1, "Blind Rivet Gun")
+            self.addTool(dbConnection, 3, 1, "Stretcher Shrinker")
+            self.addTool(dbConnection, 4, 1, "3D printers")
+
+            self.addTool(dbConnection, 5, 2, "Power Hand Drill")
+            self.addTool(dbConnection, 6, 2, "Solder Iron")
+            self.addTool(dbConnection, 7, 2, "Dremel")
+
+            self.addTool(dbConnection, 8, 3, "Horizontal Band Saw")
+            self.addTool(dbConnection, 9, 3, "Drill Press")
+            self.addTool(dbConnection, 10, 3, "Grinder / Sander")
+
+            self.addTool(dbConnection, 11, 4, "Scroll Saw")
+            self.addTool(dbConnection, 12, 4, "Table Mounted Jig Saw")
+            self.addTool(dbConnection, 13, 4, "Vertical Band Saw")
+            self.addTool(dbConnection, 14, 4, "Jig Saw")
+
+            self.addTool(dbConnection, 15, 5, "CNC router")
+            self.addTool(dbConnection, 16, 5, "Metal Lathe")
+            self.addTool(dbConnection, 17, 5, "Table Saw")
+            self.addTool(dbConnection, 18, 5, "Power Miter Saw")
+
+            dbConnection.execute('''CREATE TABLE certifications
+                                 (user_id       TEXT PRIMARY KEY,
+                                  tool_id       INTEGER,
+                                  certifier_id  TEXT,
+                                  change        TIMESTAMP,
+                                  level         INTEGER default 0)''')
+
+    def parseCert(self, str):
+        str = str.upper()
+        for level in ['BASIC', 'CERTIFIED', 'DOF', 'INSTRUCTOR', 'CERTIFIER']:
+            if str.startswith(level):
+                if str == level:
+                    return (level, '')
+                return (level, str[len(level) + 1:])
+        return ('INVALID', '')
+
+    def importFromCSV(self, filename):
+        tool_dict = {4: 1, 5: 2, 6: 3, 7: 4, 9: 5, 10: 6, 11: 7, 13: 8, 14: 9,
+                     15: 10, 17: 11, 18: 12, 19: 13, 20: 14, 22: 15, 23: 16, 24: 17, 25: 18}
+        with open(filename) as csvfile:
+            reader = csv.reader(csvfile)
+            cert_level = 0
+            cert_date = ''
+            for row in reader:
+                print('Name: ', row[2])
+                # TODO: See if name is legit, otherwise complain loudly
+                for i in range(4, 26):
+                    if row[i] and row[i] != 'N/A':
+                        print(tool_dict[i], self.parseCert(row[i]))
+
+
+        # unit test
+if __name__ == "__main__":  # pragma no cover
+    DB_STRING = 'data/test.db'
+    try:
+        os.remove(DB_STRING)   # Start with a new one
+    except:
+        pass  # Don't care if it didn't exist
+    certifications = Certifications(DB_STRING)
+    certifications.createTable()
+    certifications.importFromCSV("test.csv")
