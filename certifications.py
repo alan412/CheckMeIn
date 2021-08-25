c37a4864a26e001dd89aa690e227f2b59b3a58b8
Alan Smith <alan@randomsmiths.com>
2021-08-24 22:16:12 -0400

    add email on certification

--- certifications.py
+++ certifications.py
@@ -3,6 +3,9 @@ import sqlite3
 import os
 import csv
 import members
+import email.utils
+from email.mime.text import MIMEText
+
 from enum import IntEnum
 
 
@@ -219,3 +222,20 @@ class Certifications(object):
 
     def getLevelName(self, level):
         return self.levels[CertificationLevels(int(level))]
+
+    def emailCertifiers(self, name, toolName, levelDescription, certifierName):
+        emailAddress = "shopcertifiers@theforgeinitiative.org"
+        msg = MIMEText(f"{name} was just certified as {levelDescription} on {toolName} by {certifierName}!!")
+
+        from_email = 'tfi@ev3hub.com'
+        msg['To'] = email.utils.formataddr(("Shop Certifiers", emailAddress))
+        msg['From'] = email.utils.formataddr(('TFI CheckMeIn', from_email))
+        msg['Subject'] = 'New Certification'
+
+        try:  # pragma: no cover
+            server = smtplib.SMTP('localhost')
+            server.sendmail(from_email, [emailAddress], msg.as_string())
+            server.quit()
+        except IOError:
+            print('Email would have been:', msg)
+        return ''
