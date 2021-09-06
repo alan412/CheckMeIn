4ecac6ae74eb9bca77d8bb6cbe2d4be4f1c39cee
Alan Smith <alan@randomsmiths.com>
2021-09-06 15:52:02 -0400

    Fixed formatting

--- tests/sampleData.py
+++ tests/sampleData.py
@@ -2,13 +2,11 @@ import datetime
 
 
 def testData():
-    return {
+    return{
         "visits": [
             {
-                "start": datetime.datetime(year=2021, month=7, day=15,
-                                           hour=11),
-                "leave": datetime.datetime(year=2021, month=7, day=15,
-                                           hour=13),
+                "start": datetime.datetime(year=2021, month=7, day=15, hour=11),
+                "leave": datetime.datetime(year=2021, month=7, day=15, hour=13),
                 "barcode": "100091",
                 "status": "Out"
             },
@@ -19,30 +17,23 @@ def testData():
             },
         ],
         "members": [{
-            "barcode":
-            "100091",
-            "displayName":
-            "Member N",
-            "firstName":
-            "Member",
-            "lastName":
-            "Name",
-            "email":
-            "fake@email.com",
-            "membershipExpires":
-            datetime.datetime(year=2022, month=7, day=30)
+            "barcode": "100091",
+            "displayName": "Member N",
+            "firstName": "Member",
+            "lastName": "Name",
+            "email": "fake@email.com",
+            "membershipExpires": datetime.datetime(year=2022, month=7, day=30),
         }, {
-            "barcode":
-            "100032",
-            "displayName":
-            "Average J",
-            "firstName":
-            "Average",
-            "lastName":
-            "Joe",
-            "email":
-            "fake@email.com",
-            "membershipExpires":
-            datetime.datetime(year=2022, month=7, day=30)
-        }]
-    }
+            "barcode": "100032",
+            "displayName": "Average J",
+            "firstName": "Average",
+            "lastName": "Joe",
+            "email": "fake@email.com",
+            "membershipExpires": datetime.datetime(year=2022, month=7, day=30)
+        }],
+        "accounts": [{
+            "user": "admin",
+            "password": "password",
+            "barcode": "100091",
+            "role": 0xFF
+        }]}
